7238927a82e51017381722cef952fa39eb7acb4e
venkat <venki14101996@gmail.com>
2020-08-20 11:26:04 +0530

    orginal project

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,265 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(MaterialApp(
+  home:login() ,
+));
+
+String value;
+
+class login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+          body: Center(
+              child: loginuser()
+          )
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class loginuser extends StatefulWidget {
 
-  final String title;
+  loginuserState createState() => loginuserState();
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class loginuserState extends State {
 
-  void _incrementCounter() {
+  // Boolean variable for CircularProgressIndicator.
+  bool visible = false ;
+
+  // Getting value from TextField widget.
+  final mobileController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future loginuser() async{
+
+    // Showing CircularProgressIndicator.
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      visible = true ;
     });
+
+    // Getting value from Controller
+    String mobile = mobileController.text;
+    String password = passwordController.text;
+
+    // SERVER API URL
+    var url = 'http://vnplad.com/service2go/login_user.php';
+
+    // Store all data with Param Name.
+    var data = { 'mobile': mobile, 'password' : password, };
+
+    // Starting Web API Call.
+    var response = await http.post(url, body: json.encode(data));
+
+    // Getting Server response into variable.
+    var message = jsonDecode(response.body);
+
+    // If Web call Success than Hide the CircularProgressIndicator.
+    if(response.statusCode == 200){
+      setState(() {
+        visible = false;
+      });
+    }
+
+    // Showing Alert Dialog with Response JSON Message.
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text(message),
+          actions: <Widget>[
+            FlatButton(
+              child: new Text("OK"),
+              onPressed: () {
+
+                Navigator.of(context).pop();
+
+              },
+            ),
+          ],
+        );
+      },
+    );
+    if(message == 'Login Successfull') {
+      Navigator.pushReplacementNamed(context, "/listpage");
+    }
+    else{
+      Navigator.pushReplacementNamed(context, "/service_accept");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.0, bottom: 50.0),
+
+                  ),
+                  new SizedBox(height: 30.0,),
+
+                  new Image.asset(
+                    "assets/images/logo.png",
+                    height: 150.0,
+                    width: 210.0,
+                    fit: BoxFit.scaleDown,
+                  ),
+
+                  Divider(),
+
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30.0),
+
+                  ),
+
+                  Container(
+                      width: 280,
+                      padding: EdgeInsets.all(10.0),
+                      child: TextField(
+                        controller: mobileController,
+                        inputFormatters: [
+                          new LengthLimitingTextInputFormatter(10),
+                        ],
+                        autocorrect: true,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            labelText: "Mobile Number*"),
+                        onChanged: (text){
+                          value = text;
+                        },
+                      )
+                  ),
+
+                  Container(
+                      width: 280,
+                      padding: EdgeInsets.all(10.0),
+                      child: TextField(
+                        controller: passwordController,
+                        autocorrect: true,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Password*"),
+                      )
+                  ),
+
+
+                  new Padding(
+                    padding: EdgeInsets.only(
+                        left: 0.0, top: 20.0, bottom: 20.0),
+                    child: new RaisedButton(
+                      shape: new RoundedRectangleBorder(
+                          borderRadius:
+                          new BorderRadius.circular(30.0)),
+                      onPressed: () {
+                        if (!(mobileController.value.text
+                            .trim()
+                            .toString()
+                            .length >
+                            1)) {
+                          Fluttertoast.showToast(
+                              msg: "Please enter Mobile Number.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIosWeb: 1);
+                        } else if (!(passwordController.value.text
+                            .trim()
+                            .toString()
+                            .length >
+                            1)) {
+                          Fluttertoast.showToast(
+                              msg: "Please enter the Password.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIosWeb: 1);
+                        } else {
+
+
+                          /* Fluttertoast.showToast(
+                                              msg:
+                                              "You have successfull logedin to " +
+                                                  email_controller.value.text
+                                                      .toString(),
+                                              toastLength: Toast.LENGTH_SHORT,
+                                              gravity: ToastGravity.CENTER,
+                                              timeInSecForIos: 1);
+*/
+                          // email_controller.clear();
+                          //password_controller.clear();
+                          //Navigator.of(context).pop(LOGIN_SCREEN);
+
+                          loginuser();
+
+
+
+                        }
+                      },
+                      child: new Text(
+                        "Login",
+                        style: new TextStyle(
+                            fontWeight: FontWeight.bold),
+                      ),
+                      color: Color(0xFF54C5F8),
+                      textColor: Colors.white,
+                      elevation: 5.0,
+                      padding: EdgeInsets.only(
+                          left: 80.0,
+                          right: 80.0,
+                          top: 15.0,
+                          bottom: 15.0),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: (){
+                      //forgot password screen
+                      Navigator.pushReplacementNamed(context, "/passwordchange");
+                    },
+                    textColor: Colors.blue,
+                    child: Text('Forgot Password'),
+                    padding: EdgeInsets.only(
+                        left: 80.0,
+                        right: 80.0,
+                        top: 15.0,
+                        bottom: 15.0),
+                  ),
+
+                  FlatButton(
+                    onPressed: (){
+                      //forgot password screen
+                      Navigator.pushReplacementNamed(context, "/registerpage");
+                    },
+                    textColor: Colors.blue,
+                    child: Text('New member? Register Here',
+                      style: TextStyle(
+                          decoration:
+                          TextDecoration.underline,
+                          fontSize: 15.0),),
+                  ),
+
+
+                  Visibility(
+                      visible: visible,
+                      child: Container(
+                          margin: EdgeInsets.only(bottom: 30),
+                          child: CircularProgressIndicator()
+                      )
+                  ),
+
+                ],
+              ),
+            )));
   }
 }
